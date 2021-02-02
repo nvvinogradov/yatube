a718856c4bc4db434d75d79d652b7678a85b4cca
nvvinogradov <nvvinogradov.work@gmail.com>
2021-02-02 18:13:00 +0300

    test

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,5 +3,15 @@ from .models import Post
 from .models import Group
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("text", "pub_date", "author")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",)
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.TextField()
-    slug = models.TextField()
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, null=False)
     description = models.TextField()
 
     def __str__(self):
@@ -18,4 +18,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.text
 
